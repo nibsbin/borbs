aa9df25217ca275bca4e2ade8f90389f34a8b75d
nibs <jpthek9@gmail.com>
2026-04-18 08:01:54 -0500

    add guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,30 @@
+use rand::RngExt;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
-    println!("Guess ze numbah");
-    println!("Input ur guess");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("wtffff");
-    println!("You guessed {guess}")
+    let mut rng = rand::rng();
+    let secret_number = rng.random_range(1..=10);
+    println!("Secret number: 69420. Jk, no cheating");
+
+    loop {
+        println!("Input ur guess");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("wtffff");
+        println!("You guessed {guess}");
+
+        //convert to num
+        let guess: u32 = match guess.trim().parse() {
+            Ok(asdf) => asdf,
+            Err(_) => {println!("must be positive integer"); continue;}
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("tew smol"),
+            Ordering::Greater => println!("tew big"),
+            Ordering::Equal => {
+                println!("just right");
+                break;
+            }
+        }
+    }
 }
